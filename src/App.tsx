456f6093cd0d9174f32b96d7e06f0f77d9940ce5
yashimwong <yashimwong@gmail.com>
2021-03-10 07:03:55 +0800

    Initial Card Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Column } from "./Column";
+import { Card } from "./Card";
 import { AppContainer } from "./styles";
 
 const App = () => {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do 1">
+        <Card text="Hello there! This is a text props" />
+      </Column>
+      <Column text="To Do 2">
+        <Card text="Hello there! This is a text props" />
+      </Column>
+      <Column text="To Do 3">
+        <Card text="Hello there! This is a text props" />
+      </Column>
+    </AppContainer>
+  );
 };
 
 export default App;
